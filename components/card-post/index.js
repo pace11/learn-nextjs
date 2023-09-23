dabcc0f3cdaa3ebac107a64a5459ff275aa51e01
pace11 <ryanjoker87@gmail.com>
2023-09-24 03:11:43 +0700

    feat: init app

---
+++ components/card-post/index.js
@@ -0,0 +1,17 @@
+import styles from "./styles.module.css";
+
+export default function CardPost() {
+  return (
+    <div className={styles.card}>
+      <h1 className={styles.text}>Title Card</h1>
+      <p className={styles.text}>
+        Id elit eiusmod deserunt do ut ullamco elit ut reprehenderit labore
+        laboris duis. Magna veniam est veniam nostrud proident adipisicing. Quis
+        nisi pariatur adipisicing eiusmod enim eu enim mollit sit proident
+        magna. Proident qui deserunt tempor eu consequat. Ipsum commodo
+        excepteur tempor culpa Lorem. Nisi nisi commodo amet elit eiusmod
+        officia elit proident velit et fugiat deserunt.
+      </p>
+    </div>
+  );
+}
---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+import Layout from "@/layout";
+
+export default function Profile() {
+  return (
+    <>
+      <Layout
+        metaTitle="Profile"
+        metaDescription="Semua informasi ini adalah seputar profile user"
+      >
+        <p>Profile</p>
+      </Layout>
+    </>
+  );
+}
