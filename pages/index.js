8f681a34d5be1124f3cd5e37b1397c0d54ee86b0
pace11 <ryanjoker87@gmail.com>
2023-10-02 00:55:28 +0700

    feat: implementation api routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 const LayoutComponent = dynamic(() => import("@/layout"));
 
 export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+      .catch((err) => console.log("err => ", err));
+  }, []);
+
   return (
     <>
       <LayoutComponent metaTitle="Home">
