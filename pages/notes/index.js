692ff3ef26a578bd99c7a482ecc2e5c2ead23575
pace11 <ryanjoker87@gmail.com>
2023-10-11 01:45:23 +0700

    feat: implementation custome hooks

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -11,13 +11,18 @@ import {
   Heading,
   Text,
   Button,
+  Spinner,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQueries } from "@/hooks/useQueries";
 
 const LayoutComponent = dynamic(() => import("@/layout"));
 
 export default function Notes() {
+  const { data, isLoading } = useQueries({
+    prefixUrl: "https://paace-f178cafcae7b.nevacloud.io/api/notes",
+  });
   const router = useRouter();
   const [notes, setNotes] = useState();
 
@@ -36,17 +41,6 @@ export default function Notes() {
     } catch (error) {}
   };
 
-  useEffect(() => {
-    async function fetchingData() {
-      const res = await fetch(
-        "https://paace-f178cafcae7b.nevacloud.io/api/notes"
-      );
-      const listNotes = await res.json();
-      setNotes(listNotes);
-    }
-    fetchingData();
-  }, []);
-
   return (
     <>
       <LayoutComponent metaTitle="Notes">
@@ -59,38 +53,50 @@ export default function Notes() {
               Add Notes
             </Button>
           </Flex>
-          <Flex>
-            <Grid templateColumns="repeat(3, 1fr)" gap={5}>
-              {notes?.data?.map((item) => (
-                <GridItem>
-                  <Card>
-                    <CardHeader>
-                      <Heading>{item?.title}</Heading>
-                    </CardHeader>
-                    <CardBody>
-                      <Text>{item?.description}</Text>
-                    </CardBody>
-                    <CardFooter justify="space-between" flexWrap="wrap">
-                      <Button
-                        onClick={() => router.push(`/notes/edit/${item?.id}`)}
-                        flex="1"
-                        variant="ghost"
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        onClick={() => HandleDelete(item?.id)}
-                        flex="1"
-                        colorScheme="red"
-                      >
-                        Delete
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                </GridItem>
-              ))}
-            </Grid>
-          </Flex>
+          {isLoading ? (
+            <Flex alignItems="center" justifyContent="center">
+              <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="xl"
+              />
+            </Flex>
+          ) : (
+            <Flex>
+              <Grid templateColumns="repeat(3, 1fr)" gap={5}>
+                {data?.data?.map((item) => (
+                  <GridItem>
+                    <Card>
+                      <CardHeader>
+                        <Heading>{item?.title}</Heading>
+                      </CardHeader>
+                      <CardBody>
+                        <Text>{item?.description}</Text>
+                      </CardBody>
+                      <CardFooter justify="space-between" flexWrap="wrap">
+                        <Button
+                          onClick={() => router.push(`/notes/edit/${item?.id}`)}
+                          flex="1"
+                          variant="ghost"
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          onClick={() => HandleDelete(item?.id)}
+                          flex="1"
+                          colorScheme="red"
+                        >
+                          Delete
+                        </Button>
+                      </CardFooter>
+                    </Card>
+                  </GridItem>
+                ))}
+              </Grid>
+            </Flex>
+          )}
         </Box>
       </LayoutComponent>
     </>
